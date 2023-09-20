a89da03cbf154f10aa0c0070fc3782784fb625b4
rizkyyilman <nuugrahaaa@gmail.com>
2023-09-21 03:02:41 +0700

    feature home page commit v2

--- app/src/main/java/com/catnip/challange3/data/MenuDataResource.kt
+++ app/src/main/java/com/catnip/challange3/data/MenuDataResource.kt
@@ -13,28 +13,28 @@ class MenuDataSourceImpl() : MenuDataSource {
             position = 1,
             name = "Burger",
             price = 15.000,
-            imgUrl = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fbakeitwithlove.com%2Fwp-content%2Fuploads%2F2021%2F05%2FMcDonalds-The-Travis-Scott-Burger-sq.jpg&tbnid=3aZ9CUFAZGiEhM&vet=12ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY..i&imgrefurl=https%3A%2F%2Fid.bakeitwithlove.com%2Ftravis-scott-burger%2F&docid=E0GqRA_3Y5HUYM&w=1200&h=1200&q=burger&ved=2ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY",
+            imgUrl = "https://images.bisnis.com/posts/2017/06/22/665183/tony-stark.jpg",
             desc = "Makanan Terenak Sejagat Raya"
         ),
         DetailMenu(
             position = 2,
             name = "Ayam Goyeng",
             price = 20.000,
-            imgUrl = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fbakeitwithlove.com%2Fwp-content%2Fuploads%2F2021%2F05%2FMcDonalds-The-Travis-Scott-Burger-sq.jpg&tbnid=3aZ9CUFAZGiEhM&vet=12ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY..i&imgrefurl=https%3A%2F%2Fid.bakeitwithlove.com%2Ftravis-scott-burger%2F&docid=E0GqRA_3Y5HUYM&w=1200&h=1200&q=burger&ved=2ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY",
+            imgUrl = "https://cdns.klimg.com/resized/1200x600/p/headline/ini-alasan-kenapa-captain-america-dibua-87ae07.jpg",
             desc = "Makanan Terenak Sejagat Raya"
         ),
         DetailMenu(
             position = 3,
             name = "Dumpling",
             price = 10.000,
-            imgUrl = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fbakeitwithlove.com%2Fwp-content%2Fuploads%2F2021%2F05%2FMcDonalds-The-Travis-Scott-Burger-sq.jpg&tbnid=3aZ9CUFAZGiEhM&vet=12ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY..i&imgrefurl=https%3A%2F%2Fid.bakeitwithlove.com%2Ftravis-scott-burger%2F&docid=E0GqRA_3Y5HUYM&w=1200&h=1200&q=burger&ved=2ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY",
+            imgUrl = "https://media.gq-magazine.co.uk/photos/63468efef4f48bee2acb7062/16:9/w_2560%2Cc_limit/Tom-Holland-Spiderman-what-we-know-so-far.jpg",
             desc = "Makanan Terenak Sejagat Raya"
         ),
         DetailMenu(
             position = 4,
             name = "Mie Goreng",
             price = 25.000,
-            imgUrl = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fbakeitwithlove.com%2Fwp-content%2Fuploads%2F2021%2F05%2FMcDonalds-The-Travis-Scott-Burger-sq.jpg&tbnid=3aZ9CUFAZGiEhM&vet=12ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY..i&imgrefurl=https%3A%2F%2Fid.bakeitwithlove.com%2Ftravis-scott-burger%2F&docid=E0GqRA_3Y5HUYM&w=1200&h=1200&q=burger&ved=2ahUKEwjsqYbhpbeBAxV7mWMGHfT0BXcQMygBegQIARBY",
+            imgUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Scarlett_Johansson_as_Black_Widow.jpg/220px-Scarlett_Johansson_as_Black_Widow.jpg",
             desc = "Makanan Terenak Sejagat Raya"
         )
     )
--- app/src/main/java/com/catnip/challange3/fragmenthome/FragmentHomePage.kt
+++ app/src/main/java/com/catnip/challange3/fragmenthome/FragmentHomePage.kt
@@ -1,17 +1,42 @@
 package com.catnip.challange3.fragmenthome
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.catnip.challange3.R
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.catnip.challange3.data.MenuDataSource
+import com.catnip.challange3.data.MenuDataSourceImpl
 import com.catnip.challange3.databinding.FragmentHomePageBinding
+import com.catnip.challange3.fragmenthome.adapter.AdapterLayoutMode
+import com.catnip.challange3.model.DetailMenu
+import com.catnip.challange3.fragmenthome.adapter.FragmentHomePageAdapter
 
 
 class FragmentHomePage : Fragment() {
 
     private lateinit var binding: FragmentHomePageBinding
+
+
+    private val datasource: MenuDataSource by lazy {
+        MenuDataSourceImpl()
+    }
+    private val adapter: FragmentHomePageAdapter by lazy {
+        FragmentHomePageAdapter(AdapterLayoutMode.GRID) { detailMenu: DetailMenu ->
+            navigateToDetail(detailMenu)
+        }
+    }
+
+    private fun navigateToDetail(detailmenu: DetailMenu) {
+        findNavController().navigate(
+            FragmentHomePageDirections.actionFragmentHomePageToFragmentDetail()
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +46,19 @@ class FragmentHomePage : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupList()
+    }
 
-
+    private fun setupList() {
+        val layoutManager = GridLayoutManager(requireContext(), 2) // 2 adalah jumlah kolom
+        binding.rvMenu.layoutManager = layoutManager
+        binding.rvMenu.apply {
+            adapter = this@FragmentHomePage.adapter
+        }
+        adapter.submitData(datasource.getDetailMenu())
+    }
 }
+
+
